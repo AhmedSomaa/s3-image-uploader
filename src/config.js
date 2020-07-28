a558effdb515223d63e7893c5e60729674c6540d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-07-28 15:33:37 +0200

    move s3 config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+  bucketName: process.env.REACT_APP_BUCKET_NAME,
+  dirName: process.env.REACT_APP_DIR_NAME,
+  region: process.env.REACT_APP_REGION,
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+};
